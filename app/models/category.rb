aa8544f321c670ab71eb0691c5813d5ea1e35679
STUDIO SEMANTICA <m90studio@tutanota.com>
2020-08-23 22:03:12 -0400

    Adding :description to validates_presence_of Category model to try to debug postman route testing

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,5 @@ class Category < ApplicationRecord
   has_many :recipes, dependent: :destroy
 
   # validations
-  validates_presence_of :title, :created_by
+  validates_presence_of :title, :description, :created_by
 end
